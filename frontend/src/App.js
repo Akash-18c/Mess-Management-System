090822bc010d54ab6db4a6e0ded23f8e0f597218
Akash Chakraborty <your-chakrabortyakash067@gmail.com>
2026-07-07 14:08:04 +0530

    Remove borrows page from manager

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,6 @@ import ManagerPayments from './pages/manager/ManagerPayments';
 import ManagerBills from './pages/manager/ManagerBills';
 import ManagerOtherCharges from './pages/manager/ManagerOtherCharges';
 import ManagerMarketDuty from './pages/manager/ManagerMarketDuty';
-import ManagerBorrows from './pages/manager/ManagerBorrows';
 import MemberLayout from './pages/member/MemberLayout';
 import MemberDashboard from './pages/member/MemberDashboard';
 import MemberHistory from './pages/member/MemberHistory';
@@ -84,7 +83,6 @@ export default function App() {
           <Route path="bills" element={<ManagerBills />} />
           <Route path="charges" element={<ManagerOtherCharges />} />
           <Route path="market-duty" element={<ManagerMarketDuty />} />
-          <Route path="borrows" element={<ManagerBorrows />} />
         </Route>
 
         <Route path="/member" element={<ProtectedRoute roles={['member', 'manager', 'admin']}><MemberLayout /></ProtectedRoute>}>
